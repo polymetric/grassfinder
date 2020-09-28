57ee3a4637dbf741386a1b93266fe70d8af91353
polymetric <polymetricofficial@gmail.com>
2020-09-27 21:09:34 -0400

    file loading works

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,26 @@
 use spiral::*;
 use std::collections::HashMap;
+use std::fs;
+use std::error::Error;
+
+const PATH: &'static str = "positions.txt";
 
 fn main() {
-	let spiral = ChebyshevIterator::new(0, 0, 64);
+	let spiral = ChebyshevIterator::new(0, 0, 2048);
+	let rows = load_grass_positions().unwrap();
 
-	let mut occ: [u32; 16] = [0; 16];
-
-	for (x, z) in spiral {
-		for y in 63..128 {
-			let o = grass_offset(x, y, z);
-
-			occ[o.x as usize] += 1;
-			occ[o.y as usize] += 1;
-			occ[o.z as usize] += 1;
-		}
+	for row in rows {
+		println!("{:?}", row);
 	}
 
-	for (i, c) in occ.iter().enumerate() {
-		println!("{:>3} {:>12}", i, c);
+	for (i, (x, z)) in spiral.enumerate() {
+		for y in 63..128 {
+			let oi = grass_offset(x, y, z);
+			let of: (f64, f64, f64) = off_itof_xyz(oi.x, oi.y, oi.z);
+
+//			println!("{:>6} {:>6} {:>6} {:>12} {:>12} {:>12}", x, y, z, oi.x, oi.y, oi.z);
+//			println!("{:>6} {:>6} {:>6} {:>12.6} {:>12.6} {:>12.6}", x, y, z, of.0, of.1, of.2);
+		}
 	}
 }
 
@@ -36,6 +39,24 @@ fn grass_offset(x: i32, y: i32, z: i32) -> Offset {
 	}
 }
 
+fn off_itof_xyz(x: u8, y: u8, z: u8) -> (f64, f64, f64) {
+	(
+		map(x as f64, 0.0, 15.0, -0.25, 0.25),
+		map(y as f64, 0.0, 15.0, -0.20, 0.00),
+		map(z as f64, 0.0, 15.0, -0.25, 0.25),
+	)
+}
+
+fn map(
+	x: f64,
+	in_min: f64,
+	in_max: f64,
+	out_min: f64,
+	out_max: f64
+) -> f64 {
+	(x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+}
+
 #[derive(Copy, Clone, Debug)]
 struct Position {
 	x: i32,
@@ -48,4 +69,28 @@ struct Offset {
 	x: u8,
 	y: u8,
 	z: u8,
+}
+
+fn load_grass_positions() -> Result<Vec<(Position, Offset)>, Box<dyn Error>> {
+	let contents = fs::read_to_string(PATH)?;
+
+	Ok(contents
+		.lines()
+		.map(|line| {
+			let mut line = line.split(" ");
+
+			(
+				Position {
+					x: line.next().unwrap().parse::<i32>().unwrap(),
+					y: line.next().unwrap().parse::<i32>().unwrap(),
+					z: line.next().unwrap().parse::<i32>().unwrap(),
+				},
+				Offset {
+					x: line.next().unwrap().parse::<u8>().unwrap(),
+					y: line.next().unwrap().parse::<u8>().unwrap(),
+					z: line.next().unwrap().parse::<u8>().unwrap(),
+				}
+			)
+		})
+		.collect::<Vec<(Position, Offset)>>())
 }
